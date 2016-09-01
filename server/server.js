64890682b27c68f89204aa992d0cca6f3078e9a9
chihchiayang <chihchiayang@gmail.com>
2016-09-01 13:59:59 -0500

    initial commit

---
+++ server/server.js
@@ -0,0 +1,15 @@
+﻿var express = require('express');
+var path = require('path');
+
+var app = express();
+
+app.use(express.static('../client'))
+
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, '../client/index.html'));
+});
+
+
+var port = process.env.PORT || 4000;
+app.listen(port);
+console.log("Listening to port ", port);
